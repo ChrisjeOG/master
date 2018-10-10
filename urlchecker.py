3a1fbfa1b2e7d034f38a4af670583b0364423624
ChrisjeOG <42913583+ChrisjeOG@users.noreply.github.com>
2018-10-10 10:41:12 +0200

    Update urlchecker.py

--- urlchecker.py
+++ urlchecker.py
@@ -1,17 +1,18 @@
 import requests
 url = input("Which url do you want to check? ").strip()
-
+#get information from the url
 req = requests.get(url)
-
+#check if the status is ok, 200.
 code = req.status_code
 
+#if it's not 200, you quit the program
 if code != 200:
     print("Error")
     exit()
 else:
     print("You got it girl" + "\n")
 
-
+#the first ten lines. But only the code
 everything = req.text
 index = 0
 while index < 10:
